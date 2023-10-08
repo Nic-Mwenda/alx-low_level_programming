8604938a148f80f6dd179ff27c7c152ac3ea28ab
Nic-Mwenda <mwendaniky805@gmail.com>
2023-10-08 12:01:32 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: A program that prints single digits of base 10
+ * Return: 0 if success
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num <= 9; num++)
+	{
+		putchar(num + '0');
+	}
+	putchar('\n');
+	return (0);
+}
